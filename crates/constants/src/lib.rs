fb601f806342e68502e8fffe62a591afb11b52ef
Wleter <marcraftzastepczy@gmail.com>
2025-07-12 19:31:31 +0200

    add constants for abm

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -1,1 +1,7 @@
 pub mod units;
+
+/// The Bohr magneton in Hartree energy / Gauss
+pub const BOHR_MAG: f64 = 0.5 / 2.350517567e9;
+
+/// The nuclear magneton in Hartree energy / Gauss
+pub const NUCLEAR_MAG: f64 = 0.5 / 1836.0 / 2.350517567e9;
