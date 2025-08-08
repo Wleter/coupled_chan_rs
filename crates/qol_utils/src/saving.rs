bf416d056a3d71024033d51a255f32411f2c9dbe
Wleter <marcraftzastepczy@gmail.com>
2025-08-08 18:48:58 +0200

    add threaded eager saving

--- crates/qol_utils/src/saving.rs
+++ crates/qol_utils/src/saving.rs
@@ -2,104 +2,121 @@ use anyhow::{Result, anyhow};
 use log::info;
 use serde::Serialize;
 use std::{
-    fmt::LowerExp,
-    fs::{File, create_dir_all},
-    io::Write,
-    path::Path,
+    fmt::LowerExp, fs::{create_dir_all, File, OpenOptions}, io::{BufWriter, Write}, path::Path, sync::mpsc::{channel, Sender}
 };
 
-// todo! clean it up
-
-pub fn save_data(filename: &str, header: &str, data: &[Vec<impl LowerExp>]) -> Result<()> {
-    let n = data.first().expect("0 sized data is not allowed").len();
-    for values in data {
-        assert!(values.len() == n, "Same length data allowed only")
-    }
-
-    let mut path = std::env::current_dir()?;
-    path.push("data");
-    path.push(filename);
-    if path.extension().is_none() {
-        path.set_extension("dat");
-    }
-    let filepath = path.parent().ok_or(anyhow!("Could not get filepath parent"))?;
-
-    let mut buf = header.to_string();
-    for i in 0..n {
-        let line = data.iter().fold(String::new(), |s, val| s + &format!("\t{:e}", val[i]));
-
-        buf.push_str(&format!("\n{}", line.trim()));
-    }
-
-    if !Path::new(filepath).exists() {
-        create_dir_all(filepath)?;
-        info!("created path {}", filepath.display());
-    }
-
-    let mut file = File::create(&path)?;
-    file.write_all(buf.as_bytes())?;
-
-    info!("saved data on {}", path.display());
-    Ok(())
+pub struct DataSaver<D: Send> {
+    tx: Sender<D>
 }
 
-pub fn save_serialize(filename: &str, data: &impl Serialize) -> Result<()> {
-    let mut path = std::env::current_dir()?;
-    path.push("data");
-    path.push(filename);
-    path.set_extension("json");
-    let filepath = path.parent().ok_or(anyhow!("Could not get filepath parent"))?;
+impl<D: Send + Sync + 'static> DataSaver<D> {
+    pub fn new<F: Format<D> + 'static>(filename: &str, format: F, file_access: FileAccess) -> Result<Self> {
+        let (tx, rx) = channel();
 
-    let buf = serde_json::to_string(data)?;
+        let mut path = std::env::current_dir()?;
+        path.push(filename);
+        let filepath = path.parent().ok_or(anyhow!("Could not get parent path"))?;
 
-    if !Path::new(filepath).exists() {
-        create_dir_all(filepath)?;
-        info!("created path {}", filepath.display());
+        if !Path::new(&filepath).exists() {
+            create_dir_all(&filepath)?;
+            info!("created path {}", filepath.display());
+        }
+
+        let file = match file_access {
+            FileAccess::Append => {
+                OpenOptions::new()
+                    .append(true)
+                    .create(true)
+                    .open(&path)?
+            },
+            FileAccess::Create => {
+                File::create(&path)?
+            },
+        };
+
+        std::thread::spawn(move || {
+            let mut writer = BufWriter::new(file);
+            if let Some(header) = format.header() {
+                writeln!(writer, "{}", header).unwrap();
+            }
+            
+            for result in rx.iter() {
+                let formatted = format.format_data(&result);
+                writeln!(writer, "{}", formatted).unwrap();
+            }
+        });
+
+        Ok(Self { tx })
     }
 
-    let mut file = File::create(&path)?;
-    file.write_all(buf.as_bytes())?;
+    pub fn send(&self, data: D) -> Result<()> {
+        self.tx.send(data)?;
 
-    info!("saved data on {}", path.display());
-    Ok(())
+        Ok(())
+    }
 }
 
-pub fn save_spectrum(
-    filename: &str,
-    header: &str,
-    parameter: &[impl LowerExp],
-    spectra: &[Vec<impl LowerExp>],
-) -> Result<()> {
-    assert_eq!(
-        parameter.len(),
-        spectra.len(),
-        "parameters and energies have to have the same length"
-    );
+pub enum FileAccess {
+    Append,
+    Create,
+}
 
-    let mut path = std::env::current_dir()?;
-    path.push("data");
-    path.push(filename);
-    if path.extension().is_none() {
-        path.set_extension("dat");
-    }
-    let filepath = path.parent().ok_or(anyhow!("Could not get filepath parent"))?;
-
-    let mut buf = header.to_string();
-
-    for (p, e) in parameter.iter().zip(spectra.iter()) {
-        let line = e.iter().fold(format!("{p:e}"), |s, val| s + &format!("\t{val:e}"));
-
-        buf.push_str(&format!("\n{line}"))
+pub trait Format<D: ?Sized>: Send + Sync {
+    fn header(&self) -> Option<&str> {
+        None
     }
 
-    if !Path::new(filepath).exists() {
-        create_dir_all(filepath)?;
-        info!("created path {}", filepath.display());
+    fn format_data(&self, data: &D) -> String;
+}
+
+pub struct JsonFormat;
+
+impl<D: Serialize> Format<D> for JsonFormat {
+    fn format_data(&self, data: &D) -> String {
+        serde_json::to_string(data).unwrap()
+    }
+}
+
+pub struct DatFormat {
+    header: String,
+}
+
+impl DatFormat {
+    pub fn new(header: &str) -> Self {
+        Self { header: header.to_string() }
+    }
+}
+
+impl<D: LowerExp> Format<[D]> for DatFormat {
+    fn header(&self) -> Option<&str> {
+        Some(&self.header)
     }
 
-    let mut file = File::create(&path)?;
-    file.write_all(buf.as_bytes())?;
+    fn format_data(&self, data: &[D]) -> String {
+        use std::fmt::Write;
 
-    info!("saved data on {}", path.display());
-    Ok(())
+        let mut out = String::new();
+        for x in data {
+            // write! avoids intermediate String creation from format!
+            write!(out, "{x:e},").unwrap();
+        }
+        out
+    }
+}
+
+impl<D: LowerExp> Format<(D, Vec<D>)> for DatFormat {
+    fn header(&self) -> Option<&str> {
+        Some(&self.header)
+    }
+
+    fn format_data(&self, data: &(D, Vec<D>)) -> String {
+        use std::fmt::Write;
+
+        let mut out = format!("{:e}", data.0);
+        for x in &data.1 {
+            // write! avoids intermediate String creation from format!
+            write!(out, "{x:e},").unwrap();
+        }
+        out
+    }
 }
