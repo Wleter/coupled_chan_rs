cf40ac91e5cb3f8b9dcbfc1d18efe2e7953a261d
wleter <marcraftzastepczy@gmail.com>
2026-08-06 17:27:18 +0200

    start working on cli

---
+++ crates/coupled_chan_cli/src/input.rs
@@ -0,0 +1,130 @@
+use core::panic;
+use std::{fs, path::Path};
+
+use clebsch_gordan::half_integer::HalfU32;
+use json_comments::StripComments;
+use serde::{Deserialize, Serialize};
+
+pub fn read_input(path: impl AsRef<Path>) -> Input {
+    let path = path.as_ref();
+    if let Some(ext) = path.extension() {
+        let read = fs::read_to_string(path).unwrap();
+        match ext.to_str().unwrap() {
+            "toml" => toml::from_str(&read).unwrap(),
+            "json" | "jsonc" => serde_json::from_reader(StripComments::new(read.as_bytes())).unwrap(),
+            _ => panic!("Unknown file extension type")
+        }
+    } else {
+        panic!("Expected path to contain file extension")
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Input {
+    system: System,
+    calculation: Calculation,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum System {
+    AtomAtom {
+        s_a: HalfU32,
+        i_a: HalfU32,
+        s_b: HalfU32,
+        i_b: HalfU32,
+    },
+    #[serde(rename = "atom_rotor_TRAM")]
+    AtomDiatom {
+        s_a: HalfU32,
+        i_a: HalfU32,
+        s_b: HalfU32,
+        i_b: HalfU32,
+        s_c: HalfU32,
+        i_c: HalfU32,
+    },
+    Custom {
+        basis: Vec<Basis>,
+        operators: Vec<String>,
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Basis {
+    Spin(String, HalfU32),
+    QuantumNumbers(String, Vec<i32>),
+    Grid(String, Vec<f64>),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum Calculation {
+    Levels {
+        parameter: String,
+        range: Range,
+    },
+    Custom {
+        identifier: String,
+    },
+    BoundState {
+        parameter: String,
+        range: Range,
+        propagator: Propagator,
+        step: Step,
+        r_match: f64,
+    },
+    Scattering {
+        propagator: Propagator,
+        parameter: String,
+        range: Range,
+        step: Step,
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum Range {
+    Linear {
+        start: f64,
+        stop: f64,
+        n: u32,
+    },
+    Logarithmic {
+        arg_start: f64,
+        arg_stop: f64,
+        n: u32,
+    },
+    Points(Vec<f64>),
+    Composite(Vec<Range>),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum Step {
+    Fixed {
+        dr: f64
+    },
+    LocalWaveLength {
+        dr_min: f64,
+        dr_max: f64,
+        wave_ratio: f64
+    },
+    Transitioned {
+        transition_point: f64,
+        before: Box<Step>,
+        after: Box<Step>,
+    }
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Propagator {
+    RatioNumerov,
+    JohnsonLogDeriv,
+    ManolopoulosLogDeriv,
+}
--- crates/coupled_chan_cli/src/main.rs
+++ crates/coupled_chan_cli/src/main.rs
@@ -1,3 +1,37 @@
+pub mod input;
+
+use std::path::PathBuf;
+
+use clap::Parser;
+
+use crate::input::read_input;
+
+#[derive(Parser, Debug)]
+#[command(version, 
+    about = "CLI for coupled_chan package", 
+    long_about = "CLI for coupled_chan package solving coupled channel equation given input and problem source")
+]
+struct Args {
+    /// input file path
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// plugin library file path
+    #[arg(short, long)]
+    plugin: Option<PathBuf>,
+
+    /// total number of workers to divide in task
+    #[arg(short('W'), long, default_value_t = 1)]
+    workers: u32,
+
+    /// worker number for task assignment
+    #[arg(short('w'), long, default_value_t = 0)]
+    worker: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let input = read_input(&args.input);
+    println!("{input:?}");
 }
