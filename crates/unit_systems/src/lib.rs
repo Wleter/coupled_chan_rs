a5116680dba86eedebe545e7e1a3697284b7c645
wleter <marcraftzastepczy@gmail.com>
2026-09-09 10:23:18 +0200

    add fine constant

--- crates/unit_systems/src/lib.rs
+++ crates/unit_systems/src/lib.rs
@@ -89,6 +89,7 @@ pub struct FundamentalConstantsSI {
     pub electron_mass: f64,
     pub electric_vacuum_permittivity: f64,
     pub boltzmann_constant: f64,
+    pub fine_constant: f64,
 }
 
 pub const CODATA_2022: FundamentalConstantsSI = FundamentalConstantsSI {
@@ -98,4 +99,5 @@ pub const CODATA_2022: FundamentalConstantsSI = FundamentalConstantsSI {
     electron_mass: 9.109_383_713_9e-31,
     electric_vacuum_permittivity: 8.854_187_818_8e-12,
     boltzmann_constant: 1.380_649e-23,
+    fine_constant: 0.0072973525643,
 };
